a634ef1151ef56e350d717735db0992919bdab6d
Rafael Ferreira Lemes <40215566+rflfl@users.noreply.github.com>
2024-01-19 19:48:04 +0000

    feat: Criada a página de Login, e componente Input

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,36 @@
-import { Link } from "react-router-dom"
+import { MdEmail, MdLock} from "react-icons/md"
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+import { Input } from "../../components/Input"
+
+
+import { Column, Container, Wrapper, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin  } from "./styles";
+
 const Login = () => {
     return (<>
-        <h1>Login</h1>
-        <Link to="/">Voltar para home</Link>
+        <Header />
+        <Container>
+            <Column>
+                <Title>
+                   A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rápido nas empresas mais desejadas.
+                </Title>
+            </Column>
+            <Column>
+            <Wrapper>
+                <TitleLogin>Faça seu cadastro</TitleLogin>
+                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                <form>
+                    <Input placeholder="E-mail" leftIcon={<MdEmail/>}/>
+                    <Input placeholder="Senha" type="password" leftIcon={<MdLock/>}/>
+                    <Button title="Entrar" variant="secondary" />
+                </form>
+                <Row>
+                    <EsqueciText>Esqueci minha senha</EsqueciText>
+                    <CriarText>Criar conta</CriarText>
+                </Row>
+            </Wrapper>
+            </Column>
+        </Container>
     </>)
 }
 
