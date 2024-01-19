8b62aeecee2c33b7caa48bd7e0e6e846069c0aab
Rafael Ferreira Lemes <40215566+rflfl@users.noreply.github.com>
2024-01-19 14:38:15 +0000

    feat: Iniciando a página Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,32 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
+import bannerImage from "../../assets/banner.png"
+
+import { Container, TextContent, Title, TitleHighlight } from "./styles";
 
 const Home = () => {
     return (<>
         <Header />
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight>
+                        Implemente
+                        <br />
+                    </TitleHighlight>
+                    o seu futuro global agora!
+                </Title>
+                <TextContent>
+                    Domine as tecnologias utilizadas pelas empresas mais invoadoras do mundo e encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+                </TextContent>
+                <Button title="Começar agora" variant="secondary" onClick={() => null}></Button>
+            </div>
+            <div>
+                <img src={bannerImage} alt="Imagem banner" />
+            </div>
+        </Container>
     </>)
 }
 
